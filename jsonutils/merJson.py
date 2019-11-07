15b49e83fec64ff80e13da0e413c6b5074fd8f07
dingusagar <dingusagar@gmail.com>
2019-11-07 19:43:57 +0530

    more tests

--- jsonutils/merJson.py
+++ jsonutils/merJson.py
@@ -8,7 +8,6 @@ logging.basicConfig(level=logging.INFO)
 
 class MerJson:
     current_dir = os.path.dirname(os.path.realpath(__file__))
-    output_file_seq_no = 0
     extension_name = '.json'
 
     def read_json_to_dic(self, filename):
@@ -33,12 +32,12 @@ class MerJson:
 
         return dest_json
 
-    def get_output_filename(self,path_prefix):
+    def get_output_filename(self, path_prefix):
         files = self.list_files(path_prefix)
         if len(files) > 0:
 
             last_file = files[-1]
-            last_seq_no = last_file[len(path_prefix) : -1 * len(self.extension_name)]
+            last_seq_no = last_file[len(path_prefix): -1 * len(self.extension_name)]
 
             try:
                 last_seq_no = int(last_seq_no)
@@ -50,20 +49,19 @@ class MerJson:
         filename = path_prefix + str(last_seq_no + 1) + self.extension_name
         return filename
 
-    def write_file(self, output_base_name, dir, json_data,indent = 0):
+    def write_file(self, output_base_name, dir, json_data, indent=0):
         path_prefix = os.path.join(dir, output_base_name)
         filename = self.get_output_filename(path_prefix)
 
         with open(filename, 'w', encoding='utf-8') as f:
-            json.dump(json_data, f, ensure_ascii=False,indent= indent)
+            json.dump(json_data, f, ensure_ascii=False, indent=indent)
             logging.info(f' Writing merged file : {filename} ')
 
-    def merge_by_filename_prefix(self, input_base_name, output_base_name, max_file_size , dir = current_dir):
+    def merge_by_filename_prefix(self, input_base_name, output_base_name, max_file_size, dir=current_dir):
 
         path_prefix = os.path.join(dir, input_base_name)
         files = self.list_files(path_prefix)
         logging.info(f'  Json Files to process : {files} ')
-
 
         total_size = 0
         filename = files[0]
@@ -81,5 +79,4 @@ class MerJson:
             json_to_merge = self.read_json_to_dic(filename)
             dest_json_file = self.merge(src_json=json_to_merge, dest_json=dest_json_file)
 
-        self.write_file(output_base_name,dir,dest_json_file)
-
+        self.write_file(output_base_name, dir, dest_json_file)
--- test.py
+++ test.py
@@ -1,13 +1,14 @@
 import unittest
 from jsonutils.merJson import MerJson
+from unittest.mock import MagicMock
 import json
 
 
-class MerJsonTest(unittest.TestCase):
+class MerJson_MergeTest(unittest.TestCase):
     def setUp(self):
         self.merJson = MerJson
 
-    def test_merge_2(self):
+    def test_merge_2_json_with_same_key(self):
         json1 = json.loads("""{"strikers": [
                 { "name": "Alexis Sanchez", "club": "Manchester United" },
                 { "name": "Robin van Persie", "club": "Feyenoord" }
@@ -30,7 +31,7 @@ class MerJsonTest(unittest.TestCase):
         json_merged_string = json.dumps(json.loads(json_merged_string))  # removing whitespaces
         assert json_merged_string == result_string
 
-    def test_merge_2_multiple_keys(self):
+    def test_merge_2_json_with_multiple_keys(self):
         json1 = json.loads("""{"strikers": [
                 { "name": "Alexis Sanchez", "club": "Manchester United" },
                 { "name": "Robin van Persie", "club": "Feyenoord" }
@@ -53,6 +54,52 @@ class MerJsonTest(unittest.TestCase):
         result_string = json.dumps(self.merJson.merge(self.merJson, json2, json1))
         json_merged_string = json.dumps(json.loads(json_merged_string))  # removing whitespaces
         assert json_merged_string == result_string
+
+    def test_merge_with_empty_json(self):
+        json1 = json.loads("""{"strikers": [
+                { "name": "Alexis Sanchez", "club": "Manchester United" },
+                { "name": "Robin van Persie", "club": "Feyenoord" }
+                ] }""")
+
+        json2 = json.loads("""{}""")
+
+        json_merged_string = json.dumps(json1)
+
+        result_string = json.dumps(self.merJson.merge(self.merJson, json2, json1))
+        json_merged_string = json.dumps(json.loads(json_merged_string))  # removing whitespaces
+        assert json_merged_string == result_string
+
+    def test_merge___non_english_chars(self):
+        json1 = json.loads("""{"strikers": [
+                { "name": "頁 - 設 - 是 - 煵 - 엌 - 嫠 - 쯦 - 案 - 煪 - ㍱ - 從 ", "club": "Manchester United" },
+                { "name": "浤 - 搰 - ㍭ - 煤 - 洳", "club": "Feyenoord" }
+                ] }""")
+
+        json2 = json.loads("""
+                {"strikers": [
+                { "name": "頁Nicolas Pepe", "club": "Arsenal" }
+                ] }""")
+
+        json_merged_string = """
+        {"strikers": [
+                { "name": "頁 - 設 - 是 - 煵 - 엌 - 嫠 - 쯦 - 案 - 煪 - ㍱ - 從 ", "club": "Manchester United" },
+                { "name": "浤 - 搰 - ㍭ - 煤 - 洳", "club": "Feyenoord" },
+                { "name": "頁Nicolas Pepe", "club": "Arsenal" }
+                ] }
+        """
+
+        result_string = json.dumps(self.merJson.merge(self.merJson, json2, json1))
+        json_merged_string = json.dumps(json.loads(json_merged_string))  # removing whitespaces
+        assert json_merged_string == result_string
+
+
+# class MerJson_SizeLimitsTest(unittest.TestCase):
+#     def setUp(self):
+#         self.merJson = MerJson
+#
+#     def test_max_size_limit_is_not_exceeded(self):
+#
+#         self.merJson.read_json_to_dic = MagicMock(return_value=3)
 
 
 if __name__ == '__main__':
